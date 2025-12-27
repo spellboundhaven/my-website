a3ee814583d6d52aab4517c67f7f678a8ec7c0aa
Chubing Tripepi <chubingt.spotify.com>
2025-12-26 21:52:10 -0500

    fix: set playground photo position to 10%
    - Aggressive positioning to show maximum amount of top
    - Should fully reveal playground equipment at the top

--- components/Amenities.tsx
+++ components/Amenities.tsx
@@ -47,7 +47,7 @@ export default function Amenities() {
                     className="object-cover"
                     style={
                       images[currentImageIndex].src.includes('playground.jpg')
-                        ? { objectPosition: 'center 25%' }
+                        ? { objectPosition: 'center 10%' }
                         : undefined
                     }
                     priority={currentImageIndex === 0}
